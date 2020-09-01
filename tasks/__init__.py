e859927d284c35612ea645be748d2c2a5e20b782
drlego9 <drlego9@gmail.com>
2020-09-01 22:17:51 +0900

    remove redundant imports

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from .classification import Classification
-from .denoising import Denoising
-from .rotation import Rotation
 from .pirl import PIRL
-from .bigan import BiGAN
-from .jigsaw import Jigsaw
